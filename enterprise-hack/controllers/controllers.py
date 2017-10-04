ead73df02f37f4e7d9318a68e7dc9fcdeca2779a
Mehrdad <mehrdadbahri71@gmail.com>
2017-10-04 16:42:27 +0330

    Added get_file route

--- enterprise-hack/controllers/controllers.py
+++ enterprise-hack/controllers/controllers.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo.http import request
 
-# class Enterprise-hack(http.Controller):
-#     @http.route('/enterprise-hack/enterprise-hack/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/enterprise-hack/enterprise-hack/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('enterprise-hack.listing', {
-#             'root': '/enterprise-hack/enterprise-hack',
-#             'objects': http.request.env['enterprise-hack.enterprise-hack'].search([]),
-#         })
-
-#     @http.route('/enterprise-hack/enterprise-hack/objects/<model("enterprise-hack.enterprise-hack"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('enterprise-hack.object', {
-#             'object': obj
-#         })
+class Enterprise_hack(http.Controller):
+    @http.route('/enterprise-hack/get_file/', auth='public')
+    def index(self, **kw):
+        f = open("enterprise.tar.gz")
+        if not f:
+            return request.not_found()
+        return request.make_response(
+            f,
+            [('Content-Type', 'application/octet-stream')]
+        )
